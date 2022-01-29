1c67f7418bff81dfee9f1b66d2c0a51072373c03
sena_kuzuoka <senakuzuoka@gmail.com>
2022-01-29 22:46:27 +0900

    Add ToVector3 methods

--- Assets/Runtime/Vector2Extensions.cs
+++ Assets/Runtime/Vector2Extensions.cs
@@ -17,5 +17,37 @@ namespace TarakoKutibiru.UnityExtensions
             vector2.y = y;
             return vector2;
         }
+
+        #region ToVector3
+        public static Vector3 ToVector3XY0(this Vector2 vector2)
+        {
+            return new Vector3(vector2.x, vector2.y, 0);
+        }
+
+        public static Vector3 ToVector3YX0(this Vector2 vector2)
+        {
+            return new Vector3(vector2.y, vector2.x, 0);
+        }
+
+        public static Vector3 ToVector30XY(this Vector2 vector2)
+        {
+            return new Vector3(0, vector2.x, vector2.y);
+        }
+
+        public static Vector3 ToVector30YX(this Vector2 vector2)
+        {
+            return new Vector3(0, vector2.y, vector2.x);
+        }
+
+        public static Vector3 ToVector3X0Y(this Vector2 vector2)
+        {
+            return new Vector3(vector2.x, 0, vector2.y);
+        }
+
+        public static Vector3 ToVector3Y0X(this Vector2 vector2)
+        {
+            return new Vector3(vector2.y, 0, vector2.x);
+        }
+        #endregion
     }
 }
