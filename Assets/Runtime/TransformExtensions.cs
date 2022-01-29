12d5e490cc273617a455ed0e9e09abd25e72cef5
sena_kuzuoka <senakuzuoka@gmail.com>
2022-01-30 00:46:29 +0900

    Add TransformExtention method

--- Assets/Runtime/TransformExtensions.cs
+++ Assets/Runtime/TransformExtensions.cs
@@ -11,5 +11,35 @@ namespace TarakoKutibiru.UnityExtensions
             child.transform.parent = transform;
             return transform;
         }
+
+        public static Transform SetPosition(this Transform transform, Vector3 position)
+        {
+            transform.position = position;
+            return transform;
+        }
+
+        public static Transform SetLocalPosition(this Transform transform, Vector3 localPosition)
+        {
+            transform.localPosition = localPosition;
+            return transform;
+        }
+
+        public static Transform SetRotation(this Transform transform, Quaternion rotation)
+        {
+            transform.rotation = rotation;
+            return transform;
+        }
+
+        public static Transform SetLocalRotation(this Transform transform, Quaternion localRotation)
+        {
+            transform.localRotation = localRotation;
+            return transform;
+        }
+
+        public static Transform SetLocalScale(this Transform transform, Vector3 localScale)
+        {
+            transform.localScale = localScale;
+            return transform;
+        }
     }
 }
